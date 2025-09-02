792f265646e54174046cbfec4e55fd3aebf9a227
Ryan <ryanwjnpark@gmail.com>
2025-09-01 23:24:23 -0500

    add Suspense wrapper to {children}

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import Navbar from './components/navbar/navbar';
+import { Suspense } from 'react';
 
 const geistSans = Geist({
 	variable: '--font-geist-sans',
@@ -32,7 +33,7 @@ export default function RootLayout({
 					<Navbar />
 				</header>
 				<hr />
-				{children}
+				<Suspense>{children}</Suspense>
 			</body>
 		</html>
 	);
